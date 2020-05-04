972d87b59ab6ed63bd89cbb75d0a1f791930a8c0
Pablo <pablolavado08@gmail.com>
2020-05-04 22:00:18 +0200

    Ejercicio Final

--- 1-CasadePapel.js
+++ 1-CasadePapel.js
@@ -1,8 +1,48 @@
 /* Hacer un programa que dada una capital indique si se trata de uno de los nombres elegidos por El Profesor para alguno de los miembros de su equipo o no.
 Necesitamos tenerlo para dentro de 10 días… ¡hay demasiados rehenes en juego! */
-// OPCIÓN LARGA:
+
+// Versión más corta, en tan SOLO 10 LÍNEAS:
+
 var nombre = "";
-function atracador(miembro){alert("Alerta, " + miembro + " es un miembro de la banda de: La Casica de Papel.");}
+var atracadores = ["TOKIO", "NAIROBI", "BERLÍN", "RÍO", "DENVER", "OSLO", "HELSINKI", "MOSCÚ"];
+
+while (isNaN(nombre) === false) { nombre = prompt("Dime la capital que tienes en mente"); nombre = nombre.toUpperCase(); }
+for (var i = 0; i < atracadores.length; i++) { 
+    if (nombre === atracadores[i]) { alert("Alerta, " + nombre + " es un miembro de la banda de: La Casica de Papel.");
+    break;
+    }
+} 
+if (nombre !== atracadores[i]) { alert("La capital " + nombre + ", no corresponde al nombre de ningún miembro de la casa de papel ");}
+
+
+// --------------------------------------------------------------------------------------------------------
+// CON FUNCIONES:
+
+/* var nombre = "";
+var atracadores = ["TOKIO", "NAIROBI", "BERLÍN", "RÍO", "DENVER", "OSLO", "HELSINKI", "MOSCÚ"];
+
+ask();
+check(atracadores);
+
+function ask() {
+    while (isNaN(nombre) === false) { nombre = prompt("Dime la capital que tienes en mente"); nombre = nombre.toUpperCase();}
+    return nombre;
+} 
+function check (name) {
+    for (var i = 0; i<= name.length; i++) { 
+        if (nombre === name[i]) { alert("Alerta, " + nombre + " es un miembro de la banda de: La Casica de Papel.");
+        break;
+    }
+}
+        if (nombre !== name[i]){ alert("La capital " + nombre + ", no corresponde al nombre de ningún miembro de la casa de papel ");
+    }
+} */
+
+// OPCIÓN LARGA:
+/* 
+var nombre = "";
+function atracador(miembro) { alert("Alerta, " + miembro + " es un miembro de la banda de: La Casica de Papel.");} //Texto a mostrar en caso de detectar al atracador;
+
 while (isNaN(nombre) === false) { // Comprobar dato introducido;
     nombre = prompt("Dime la capital que tienes en mente");
     nombre = nombre.toUpperCase();
@@ -31,7 +71,7 @@ switch (nombre) {
     case "MARSELLA":
         atracador(nombre);
         break;
-    
+
     case "OSLO":
         atracador(nombre);
         break;
@@ -49,15 +89,4 @@ switch (nombre) {
         break;
 }
 
-// ------------------------------------------------------------------------------------------
-// ESTA LA MEJOR OPCIÓN:
-var nombre = "";
-var atracadores = ["TOKIO", "NAIROBI", "BERLÍN", "RÍO", "DENVER", "OSLO", "HELSINKI", "MOSCÚ"];
-while (isNaN(nombre) === false) { nombre = prompt("Dime la capital que tienes en mente"); nombre = nombre.toUpperCase(); }
-for (let i = 0; i < atracadores.length; i++) { 
-    if (nombre === atracadores[i]) { alert("Alerta, " + nombre + " es un miembro de la banda de: La Casica de Papel.");
-        break} 
-    if (nombre != atracadores.length) { alert("La capital " + nombre + ", no corresponde al nombre de ningún miembro de la casa de papel ");
-        break;
-    }
-}
+ */
